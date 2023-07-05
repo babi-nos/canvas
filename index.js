84e06f1ac53d1aedbb522a9183b0646df1e92a1e
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-07-05 17:58:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d"); // ctx = CONTEXTE
-const btns = document.querySelectorAll("button");
 
-function draw() {
-  const x = (canvas.width - 100) / 2;
-  const y = (canvas.height - 100) / 2;
-//   ctx.fillStyle = "red";
-  ctx.fillRect(x, y, 100, 100);
+function drawCirlcle() {
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+  const radius = 50;
+
+  ctx.fillStyle = "blue";
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
 }
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    switch (btn.innerHTML) {
-      case "Green":
-        ctx.fillStyle = "green";
-        draw();
-        break;
-      case "Red":
-        ctx.fillStyle = "red";
-        draw();
-        break;
-      case "Black":
-        ctx.fillStyle = "black";
-        draw();
-        break;
+drawCirlcle();
 
-      default:
-        break;
-    }
-  });
-});
+// const btns = document.querySelectorAll("button");
 
-// draw();
+// function draw() {
+//   const x = (canvas.width - 100) / 2;
+//   const y = (canvas.height - 100) / 2;
+//   ctx.clearRect(0, 0, canvas.width, canvas.height); // Efface le canevas
+//   ctx.fillRect(x, y, 100, 100);
+// }
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     switch (btn.innerHTML) {
+//       case "Green":
+//         ctx.fillStyle = "green";
+//         draw();
+//         break;
+//       case "Red":
+//         ctx.fillStyle = "red";
+//         draw();
+//         break;
+//       case "Black":
+//         ctx.fillStyle = "black";
+//         draw();
+//         break;
+
+//       default:
+//         break;
+//     }
+//   });
+// });
+
+// draw(); // Dessine le rectangle initial avec la couleur de remplissage par défaut (rouge)
